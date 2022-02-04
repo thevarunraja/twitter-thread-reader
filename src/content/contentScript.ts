628256c0d48e0cd973f5eed86188d2e4ba6d6251
varun.raja <varun.raja@outlook.com>
2022-02-03 22:37:37 -0500

    refactor

--- src/content/contentScript.ts
+++ src/content/contentScript.ts
@@ -1,63 +1,16 @@
 import "arrive";
 import checkForThemeChanges from "./checkForThemeChanges";
 import {
+  addListenersToDOM,
   checkForAddingReaderButton,
-  disableThreadReaderMode,
   enableThreadReaderMode,
   injectScriptToListenForThreadResponse,
+  getQueryParams,
 } from "./threadReader";
-
-document.addEventListener("click", function (event) {
-  // @ts-ignore
-  if (event.target && event.target?.id === "open-reader-mode") {
-    enableThreadReaderMode();
-  } // @ts-ignore
-  else if (event.target && event.target?.id === "navigate-to-view-thread") {
-    window.scroll(0, 0);
-    // @ts-ignore
-    window.location.href = `${
-      document.location.href.split("/status/")[0]
-    }/status/${
-      // @ts-ignore
-      event.target.dataset.parenttweetid
-    }?threadReaderMode=true`;
-  } else if (
-    document
-      .querySelector('[aria-label="Timeline: Conversation"]')
-      // @ts-ignore
-      ?.contains(event.target)
-  ) {
-    //
-  } else if (
-    document
-      .querySelector('[aria-labelledby="modal-header"]')
-      // @ts-ignore
-      ?.contains(event.target)
-  ) {
-    //
-  } else {
-    disableThreadReaderMode();
-  }
-});
-
-document.onkeydown = function (evt) {
-  evt = evt || window.event;
-  if (evt.keyCode == 27) {
-    disableThreadReaderMode();
-  }
-};
-
-export function getQueryParams() {
-  const params = new Proxy(new URLSearchParams(window.location.search), {
-    // @ts-ignore
-    get: (searchParams, prop) => searchParams.get(prop),
-  });
-  return params;
-}
 
 export default function init() {
   injectScriptToListenForThreadResponse();
-
+  addListenersToDOM();
   document.addEventListener("DOMContentLoaded", () => {
     checkForThemeChanges();
 
--- src/content/threadReader.ts
+++ src/content/threadReader.ts
@@ -157,3 +157,53 @@ export function disableThreadReaderMode() {
   document.getElementById("react-root")?.classList.remove("thread-reader-mode");
   checkForAddingReaderButton();
 }
+
+export function addListenersToDOM() {
+  document.addEventListener("click", function (event) {
+    // @ts-ignore
+    if (event.target && event.target?.id === "open-reader-mode") {
+      enableThreadReaderMode();
+    } // @ts-ignore
+    else if (event.target && event.target?.id === "navigate-to-view-thread") {
+      window.scroll(0, 0);
+      // @ts-ignore
+      window.location.href = `${
+        document.location.href.split("/status/")[0]
+      }/status/${
+        // @ts-ignore
+        event.target.dataset.parenttweetid
+      }?threadReaderMode=true`;
+    } else if (
+      document
+        .querySelector('[aria-label="Timeline: Conversation"]')
+        // @ts-ignore
+        ?.contains(event.target)
+    ) {
+      //
+    } else if (
+      document
+        .querySelector('[aria-labelledby="modal-header"]')
+        // @ts-ignore
+        ?.contains(event.target)
+    ) {
+      //
+    } else {
+      disableThreadReaderMode();
+    }
+  });
+
+  document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+      disableThreadReaderMode();
+    }
+  };
+}
+
+export function getQueryParams() {
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    // @ts-ignore
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+  return params;
+}
